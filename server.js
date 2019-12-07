4de6be0512b1264523d886b6e5b67cdf6c1f5197
Trần Ngọc Qúy (VINID-KCN-MB) <quytn3@vingroup.net>
2019-12-07 11:29:42 +0700

    setup db

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
+router.render = (req, res) => {
+  if (req.method !== "GET") return res.jsonp(res.locals.data);
+  let queryString = req.originalUrl.split("?")[1];
+  let queryArray = queryString && queryString.split("&");
+  if (queryArray) {
+    let queryField = queryArray.find(
+      e => e.split("=")[0] === "fields"
+    );
+    if (queryField) var fieldArray = queryField.split("=")[1].split(",");
+    if (fieldArray) return res.jsonp(res.locals.data.map(e => {
+      let obj = {};
+      fieldArray.forEach(element => {
+        obj[element] = e[element]
+      });
+      return obj;
+    }))
+  }
+  
+  
+  res.jsonp(res.locals.data);
+}
 server.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
